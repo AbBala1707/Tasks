00fd76b7312a7edf23d53b0eb1994e50e9a3e616
AbBala1707 <91530439+AbBala1707@users.noreply.github.com>
2021-10-28 09:54:07 +0530

    Add files via upload

---
+++ Charrev.java
@@ -0,0 +1,24 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Charrev {
+
+	public static void main(String[] args) {
+		
+		character();
+
+	}
+	static void character()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First Letter :");
+		char ch=sc.next().charAt(0);
+		System.out.println("Enter Second Letter :");
+		char ch1=sc.next().charAt(0);
+		System.out.println("Enter Third Letter :");
+		char ch2=sc.next().charAt(0);
+		System.out.println(ch2+""+ch1+""+ch);
+	}
+
+}
